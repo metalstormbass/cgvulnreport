80a63fa72a03129d9c7ace480cdbd7311b7c73a5
Michael Braun <michaelbraunbass@gmail.com>
2024-12-16 08:11:05 -0700

    Update json_to_markdown.sh

--- scripts/json_to_markdown.sh
+++ scripts/json_to_markdown.sh
@@ -14,7 +14,7 @@ json_data=$(cat "$1")
 echo "### **Detailed Vulnerabilty Scans**" 
 echo "This table provides a detailed breakdown of vulnerabilities per image, categorized by severity and fix availability."
 echo "<br>"
-echo "| **Image** | **Type** | **Critical** | **High** | **Medium** | **Low** | **Wont Fix** | **Total** | **Fixed Critical** | **Fixed High** | **Fixed Medium** | **Fixed Lowv | **Fixed Total** |"
+echo "| **Image** | **Type** | **Critical** | **High** | **Medium** | **Low** | **Wont Fix** | **Total** | **Fixed Critical** | **Fixed High** | **Fixed Medium** | **Fixed Low | **Fixed Total** |"
 echo "|-------|------|----------|------|--------|-----|----------|-------|----------------|------------|--------------|-----------|-------------|"
 
 # Use jq to parse the JSON data, remove the tag, and print each item in the table format
